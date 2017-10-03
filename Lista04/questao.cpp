f8b90f84f35b78746de159a82d3879c117ff880f
Rafael Mota Alves <rma7@ug3c21.windows.cin.ufpe.br>
2017-10-03 14:48:05 -0300

    aaa

--- Lista04/questao.cpp
+++ Lista04/questao.cpp
@@ -1,8 +1,11 @@
 #include<bits/stdc++.h>
 
 
+using namespace std;
+
 struct task
 {
+	int priorityO;
 	int priority;
 	int id;
 
@@ -13,6 +16,18 @@ struct CPU
 {
 	task process;
 	char status;
+	int use;
+
+	CPU(){
+		status = 'O';
+	}
+
+	void assignTask(task t){
+		process = t;
+		status = 'A';
+		use += (20 - t.priority);
+	}
+
 };
 
 
@@ -25,13 +40,18 @@ private:
 public:
 	Max_heap();
 	~Max_heap();
-	void insert(int id,int priority);
+	void insert(task t);
 	void bubbleup();
 	void doubleSize();
 	void heapify(int i);
 	task extract();
 	void print();
+	int getHeap_size();
 };
+
+int Max_heap::getHeap_size(){
+	return heap_size;
+}
 
 Max_heap::Max_heap(){
 	heap_size = 0;
@@ -43,13 +63,10 @@ Max_heap::~Max_heap(){
 	delete[] array;
 }
 
-void Max_heap::insert(int id,int priority){
+void Max_heap::insert(task t){
 	if(heap_size == array_size){
 		doubleSize();
 	}
-	task t;
-	t.id = id;
-	t.priority = priority;
 	array[heap_size] = t;
 	heap_size++;
 	bubbleup();
@@ -58,7 +75,7 @@ void Max_heap::insert(int id,int priority){
 
 void Max_heap::bubbleup(){
 	int i = heap_size - 1;
-	while(i>0 && array[i].priority > array[(int)floor((i-1)/2)].priority ){
+	while(i>0 && array[i].priority < array[(int)floor((i-1)/2)].priority ){
 			task aux = array[i];
 			array[i] = array[(int)floor((i-1)/2)];
 			array[(int)floor((i-1)/2)] = aux;
@@ -86,10 +103,10 @@ void Max_heap::heapify(int i){
 	int l = 2*i + 1;
 	int r = 2*i + 2;
 	int m = i;
-	if(l < heap_size && array[l].priority >= array[m].priority){
+	if(l < heap_size && array[l].priority <= array[m].priority){
 		m = l;
 	}
-	if(r < heap_size && array[r].priority > array[m].priority){
+	if(r < heap_size && array[r].priority < array[m].priority){
 		m = r;
 	}else if(r < heap_size && array[r].priority == array[m].priority){
 		if(array[r].id < array[m].id ){
@@ -127,24 +144,87 @@ void Max_heap::print(){
 	std::cout << "\n";
 }
 
+
 int main(){
-	Max_heap *test = new Max_heap();
+	Max_heap *heap = new Max_heap();
+	int n;
 
-	test->insert(0,5);
-	test->insert(1,6);
-	test->insert(2,9);
-	test->insert(3,0);
-	test->insert(4,9);
-	test->insert(5,5);
-	test->insert(6,9);
-
-
-	test->print();
+	cin >> n;
 
 
 
+	int contproc[10] ;//qtd de processos
+	int conttemp[10] ;//tempo por processo
+	int controd[10] ;//quantidade de rodadas por process
+
+	for(int i =0 ; i < 10 ; i++ ){
+		contproc[i] = 0;
+		conttemp[i] = 0;
+		controd[i] = 0;
+	}
+
+	CPU *cpu = new CPU[n];
 
 
+	string command;
+
+	while(true){
+		
+		cin >> command;
+
+		if(command == "END"){
+			break;
+		}else if(command == "NEW"){
+			task t;
+			int p, id;
+			cin >> id;
+			cin >> p;
+			t.priority = p;
+			t.priorityO = p;
+			t.id = id;
+			contproc[p] = contproc[p] + 1;
+			heap->insert(t);
+		}else if (command ==  "STA"){
+			int id;
+			char st;
+			cin >>id;
+			cin >>st;
+			if(st == 'A'){
+				if(cpu[id].process.priority != 9){
+					cpu[id].process.priority = cpu[id].process.priority + 1;
+				}
+				controd[cpu[id].process.priorityO]++;
+				heap->insert(cpu[id].process);
+
+			}
+			cpu[id].status = 'O';
+
+		}
+
+	
+	for(int i = 0 ; i < 9; i++){
+		if(cpu[i].status == 'O' && heap->getHeap_size() > 0){
+			task a = heap->extract();
+			conttemp[a.priorityO] += (20 - a.priority);
+			cpu[i].assignTask(a);
+		}
+	}
+	
+	}
+
+	cout << "STATS BY PRIORITY" << endl <<endl;
+
+	for(int i = 0; i < 10; i++){
+		cout << i << ": " << contproc[i] << " "  << conttemp[n] << " " << controd[n] << endl; 
+	}
+
+	cout << endl;
+
+
+	cout << "STATS BY CPU" << endl << endl;
+	for(int i = 0; i < n ; i++){
+		cout << i << ": " << cpu[i].use << endl;
+	}
 
 
 	return 0;
