42192fd3d3acb98d447057471d668ee9f43c19ef
Rafael Mota <rma7@cin.ufpe.br>
2018-05-18 23:19:21 -0300

    Fix Binary Search

--- BinarySearch/main.cpp
+++ BinarySearch/main.cpp
@@ -1,32 +1,21 @@
 #include <iostream>
 #include <cstdlib>
 #include "Sort.h"
+#include <math.h>
 
 using namespace std;
 
-int binarySearch(int v,int array[],int size);
+int binarySearch(int v,int array[],int i,int j,int size);
 
 int main(){
-	int array[] = {359,
-					966,
-					105,
-					115,
-					81,
-					255,
-					74,
-					236,
-					809,
-					205,
-					186,
-					939,
-					498,
-					763,
-					483,
-					326,
-					124,
-					706,
-					84,
-					1016};
+	int array[] = {1,
+		2,
+		3,
+		6,
+		5,
+		10,
+		12,
+		25};
 
 	Sort s;
 
@@ -40,22 +29,25 @@ int main(){
 	}
 	cout<<endl;
 
+	cout << binarySearch(2,array,0,size,size);
+
 	return 0;
 }
 
+int binarySearch(int v, int array[],int i ,int j,int size){
+	
+	int p = (j+i)/2;
 
-int binarySearch(int v,int array[],int size){
-	int l = 0;
-	int r = size-1;
-	do{
-		int a = (l + r)/2;
-		if(array[a] == v){
-			return a;
-		}else if(array[a] > v){
-			r = a-1;
-		}else{
-			l = a+1;
-		}
-	}while(l <= r);
-	return -1;
+	if(j - i == 1 && array[p] != v){
+		return -1;
+	}else if(v > array[p]){
+		return binarySearch(v,array,p+1,j,size);
+	}else if(v < array[p]){
+		return binarySearch(v,array,i,p,size);
+	}else{
+		return p;
+	}
+
 }
+
+//0 1 2 3 4
